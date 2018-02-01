d4ea60c5c2db50c4a4f5d423868b86c29c512795
Sanjeev Kumar2 <sanjeev.kumar2@olacabs.com>
2018-02-01 14:26:17 +0530

    Updated data with codeforces api

---
+++ js/cp_api/codeforces.js
@@ -0,0 +1,29 @@
+var Codeforces = {
+    userinfo_url: "http://codeforces.com/api/user.info",
+    handle: "sanjeev1779",
+
+    getUserInfo: function() {
+        final_url = Codeforces.userinfo_url + "?handles=" + Codeforces.handle
+        $.ajax({
+            url: final_url,
+            type: 'get',
+            success: function(data) {
+                if (data["status"] == "OK") {
+                    userinfo = data["result"][0];
+                    $(".codeforces .max_rating").text(userinfo["maxRating"]);
+                    $(".codeforces .max_rating_category").text(userinfo["maxRank"]);
+                    $(".codeforces .cur_rating").text(userinfo["rating"]);
+                    $(".codeforces .cur_rating_category").text(userinfo["rank"]);
+
+                }
+            },
+            error: function(data) {
+                console.log(data);
+            }
+        });
+    }
+};
+
+$(document).ready(function() {
+    Codeforces.getUserInfo()
+});
